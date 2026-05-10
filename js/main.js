a646cf8910c6f38dd489b4a71ae255ceeb34e134
abdelllahmob-cyber <abdelllahmob@gmail.com>
2026-05-10 21:24:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -104,36 +104,3 @@ document.querySelectorAll('.chart-bar').forEach(bar => {
     });
 });
 
-// HLS Video Players (Bunny.net streams)
-const initHLS = (videoId, src) => {
-    const video = document.getElementById(videoId);
-    if (!video) return;
-
-    if (typeof Hls !== 'undefined' && Hls.isSupported()) {
-        const hls = new Hls({
-            startLevel: -1,
-            enableWorker: false,
-            xhrSetup: (xhr) => { xhr.withCredentials = false; }
-        });
-        hls.loadSource(src);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
-            video.closest('.video-player-wrap, .video-glow-inner')
-                ?.classList.add('video-ready');
-        });
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = src;
-        video.load();
-    }
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    initHLS(
-        'site-demo-video',
-        'https://vz-8d74ca40-f55.b-cdn.net/3383f005-7a67-40e0-9b9a-594be2924a05/playlist.m3u8'
-    );
-    initHLS(
-        'results-video',
-        'https://vz-8d74ca40-f55.b-cdn.net/ee2094b0-c5e8-492b-ba4c-b85438849d98/playlist.m3u8'
-    );
-});
